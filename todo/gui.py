0eb6a1f5c6a9a2887d250a38c9b9826198ad979c
george <george@alland.co.uk>
2024-08-29 19:35:43 +0100

    Added Edit function

--- todo/gui.py
+++ todo/gui.py
@@ -11,24 +11,39 @@ edit_button = sg.Button("Edit")
 
 window = sg.Window("My TODO App",
                    layout=[[label],[input_box, add_button],[list_box, edit_button]],
-                    font = ("Helvetica",15))
+                    font = ("Helvetica",10))
 
 while True:
     event, values = window.read()
 
-    print(event)
-    print(values)
-    print(values["todos"])
+    print(1, event)
+    print(2, values)
+    print(3, values["todo"])
+
 
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todo"] + "\n"
+            new_todo = values["todo"] #+ "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
 
         case "Edit":
-            todo =values["todos"]
+            todo_to_edit =values["todos"][0]
+            new_todo = values["todo"]
+
+            todos=functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+
+            functions.write_todos(todos)
+
+            window["todos"].update(values=todos)
+
+        case"todos":
+            window["todo"].update(value = values["todos"][0])
+
 
         case sg.WIN_CLOSED:
             break
