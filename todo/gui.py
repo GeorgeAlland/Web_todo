7d02422cde98d66102a5e8c8dab3e796b3daaebe
george <george@alland.co.uk>
2024-09-11 20:59:54 +0100

    Added icons, Dealt with adding blank todo

--- todo/gui.py
+++ todo/gui.py
@@ -7,11 +7,14 @@ sg.theme("DarkGreen")
 clock = sg.Text ("", key="clock")
 label = sg.Text ("Type in a ToDo")
 input_box = sg.InputText(tooltip = "Enter a Todo..",key ="todo")
-add_button =sg.Button("Add",)
+#add_button =sg.Button(size = 10, image_source= "Add.png")
+add_button =sg.Button("Add", size = 10, mouseover_colors= "LightBlue2",
+                      tooltip = "Add New TODO", key="Add")
 list_box = sg.Listbox(values = functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+#complete_button = sg.Button(size = 10, image_source= "Complete.png")
+complete_button = sg.Button("Complete",size = 10)
 exit_button = sg.Button("Exit")
 
 #layout = [[label],[input_box, add_button],[list_box, edit_button]]
@@ -25,7 +28,7 @@ window = sg.Window("My TODO App",
                     font = ("Helvetica",10))
 
 while True:
-    event, values = window.read(timeout =50)
+    event, values = window.read(timeout =200)
 
 
 
@@ -36,11 +39,16 @@ while True:
 
     match event:
         case "Add":
+
             todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
+            if new_todo != "\n":
+                todos.append(new_todo)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+
+
 
         case "Edit":
             try:
@@ -56,7 +64,7 @@ while True:
 
 
             except IndexError:
-               sg.popup("Please select an Item to Edit.", font=("Helvetica", 15) )
+               sg.popup("Please select an Item to Edit.", font=("Helvetica", 10), title = "error" )
 
 
         case "Complete":
@@ -69,7 +77,7 @@ while True:
                 window["todos"].update(values=todos)
                 window["todo"].update(value = "")
             except IndexError:
-                sg.popup("Please select an Item to Complete.", font=("Helvetica", 15))
+                sg.popup("Please select an Item to Complete.", font=("Helvetica", 15),title = "Error")
 
 
         case "Exit":
