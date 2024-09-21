f3369b2198bd89bcf9efa32074d8803aeadfafe4
george <george@alland.co.uk>
2024-09-21 19:18:32 +0100

    Added icons, Dealt with a11-0dding blank todo Started with Web Interface
    added new item functionality

--- todo/web.py
+++ todo/web.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 todos = functions.get_todos()
 
 st.title("My TODO App")
@@ -12,4 +18,5 @@ st.write("random text")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add new Item...")
+st.text_input(label="",placeholder="Add new Item...",
+              on_change=add_todo, key="new_todo")
