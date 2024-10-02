6a4938687ac5f0853f83073ba4b761e7eda44ead
george <george@alland.co.uk>
2024-10-02 22:01:10 +0100

    Added Index number to TODOS

---
+++ Web_TODO/web.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import functions
+#streamlit run web.py
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+
+for index, todo in enumerate(todos):
+
+    test2 = (f"({index}) {todo}")
+    #st.warning(test2)
+    #checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(test2, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        #st.experimental_rerun()
+        st.rerun()
+
+st.text_input(label="-", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
