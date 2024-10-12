66dd385c3fb05b5dd130c1c0d342de13185b983f
george <george@alland.co.uk>
2024-10-12 22:06:44 +0100

    Update Filepath

--- Web_TODO/functions.py
+++ Web_TODO/functions.py
@@ -5,6 +5,7 @@ def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
     to-do items.
     """
+
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
 
