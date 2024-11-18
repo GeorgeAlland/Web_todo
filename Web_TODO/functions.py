84045f359b55fe85a7a56f3b180eb7e65470ab76
george <george@alland.co.uk>
2024-11-18 19:20:19 +0000

    18-11-24 Update

--- Web_TODO/functions.py
+++ Web_TODO/functions.py
@@ -11,7 +11,6 @@ def get_todos(filepath=FILEPATH):
 
     return todos_local
 
-
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
